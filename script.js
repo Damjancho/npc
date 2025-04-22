4d2cec38e70655a209a5c485887df11a9898ac1d
Damjancho <pavleski.damjan@gmail.com>
2025-04-22 20:04:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let block = document.querySelector('.rotating-img');
 
+document.addEventListener('mousemove', function(e) {
+    if (!block) return;
+
+    let dx = e.pageX - window.innerWidth / 2;
+    let dy = e.pageY - window.innerHeight / 2;
+
+    let angleX = 20 * dx / (window.innerWidth / 2);
+    let angleY = 20 * dy / (window.innerHeight / 2);
+
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`;
+});
+
+const links = document.querySelectorAll('nav ul li a');
+
+links.forEach(link => {
+    link.addEventListener('mouseover', function() {
+        link.style.color = '#FFD700'; 
+    });
+    link.addEventListener('mouseout', function() {
+        link.style.color = 'white'; 
+    });
+});
